e06f3e8720d244e94935ef45683eb4dfce540240
ryzac <rzc044@gmail.com>
2024-05-30 14:02:52 -0500

    Custom error handling.

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,13 +6,32 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
 const morgan = require("morgan");
+
+const { NotFoundError } = require("../utils/expressError");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
+
+/** Handle 404 errors. */
+app.use((req, res, next) => {
+    return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use((err, req, res, next) => {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    // default status is 500 Internal Server Error
+    const status = err.status || 500;
+    const message = err.message;
+
+    // set the status and alert the user
+    return res.status(status).json({
+        error: { message, status }
+    });
+});
 
 module.exports = app;
