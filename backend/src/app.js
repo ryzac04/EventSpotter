e4eb5c9ff413cb7a8f1215646041273b954e16a5
ryzac <rzc044@gmail.com>
2024-05-30 20:12:31 -0500

    Modified generic error handler.

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,7 +26,7 @@ app.use((err, req, res, next) => {
     if (process.env.NODE_ENV !== "test") console.error(err.stack);
     // default status is 500 Internal Server Error
     const status = err.status || 500;
-    const message = err.message;
+    const message = err.message || "Internal Server Error";
 
     // set the status and alert the user
     return res.status(status).json({
