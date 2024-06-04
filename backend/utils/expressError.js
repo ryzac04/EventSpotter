2989182a0704a6fcabfe9f58744427021e94743f
ryzac <rzc044@gmail.com>
2024-06-04 02:59:36 -0500

    Formatting change.

--- backend/utils/expressError.js
+++ backend/utils/expressError.js
@@ -6,50 +6,50 @@
  */
 
 class ExpressError extends Error{
-    constructor(message, status) {
-        super();
-        this.message = message;
-        this.status = status;
-        console.error(this.stack);
-    }
-}
-
-/** 404 NOT FOUND error. */
-
-class NotFoundError extends ExpressError{
-    constructor(message = "Not Found") {
-        super(message, 404);
-    }
-}
-
-/** 401 UNAUTHORIZED error. */
-
-class UnauthorizedError extends ExpressError {
-  constructor(message = "Unauthorized") {
-    super(message, 401);
-  }
-}
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+    console.error(this.stack);
+  };
+};
 
 /** 400 BAD REQUEST error. */
 
 class BadRequestError extends ExpressError {
   constructor(message = "Bad Request") {
     super(message, 400);
-  }
-}
+  };
+};
+
+/** 401 UNAUTHORIZED error. */
+
+class UnauthorizedError extends ExpressError {
+  constructor(message = "Unauthorized") {
+    super(message, 401);
+  };
+};
 
 /** 403 FORBIDDEN REQUEST error. */
 
 class ForbiddenError extends ExpressError {
   constructor(message = "Bad Request") {
     super(message, 403);
-  }
-}
+  };
+};
+
+/** 404 NOT FOUND error. */
+
+class NotFoundError extends ExpressError{
+  constructor(message = "Not Found") {
+    super(message, 404);
+  };
+};
 
 module.exports = {
   ExpressError,
-  NotFoundError,
-  UnauthorizedError,
   BadRequestError,
+  UnauthorizedError,
   ForbiddenError,
+  NotFoundError,
 };
