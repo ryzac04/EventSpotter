2b27a8a02b5d3882c85bbae201304a29b5a1040d
ryzac <rzc044@gmail.com>
2024-06-05 14:25:46 -0500

    Add IF EXISTS to DROP DATABASE

--- backend/db/eventspotter.sql
+++ backend/db/eventspotter.sql
@@ -1,12 +1,14 @@
 
-DROP DATABASE eventspotter_dev;
+-- run this file upon initial clone/start to create databases and seed with example data. This will help verify the databases and tables are properly set up for further tests/development. 
+
+DROP DATABASE IF EXISTS eventspotter_dev;
 CREATE DATABASE eventspotter_dev;
 \connect eventspotter_dev;
 
 \i eventspotter-schema.sql;
 \i eventspotter-seed.sql;
 
-DROP DATABASE eventspotter_test;
+DROP DATABASE IF EXISTS eventspotter_test;
 CREATE DATABASE eventspotter_test;
 \connect eventspotter_test;
 
