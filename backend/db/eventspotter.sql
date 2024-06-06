319c551f566dfcb81f19fb1902a988079ca23418
ryzac <rzc044@gmail.com>
2024-06-06 08:39:45 -0500

    Added documentation/comments.

--- backend/db/eventspotter.sql
+++ backend/db/eventspotter.sql
@@ -1,16 +1,20 @@
 
--- run this file upon initial clone/start to create databases and seed with example data. This will help verify the databases and tables are properly set up for further tests/development. 
+ -- Initializes the development and test databases for the EventSpotter application by creating the necessary databases, applying schema changes, and seeding them with example data.
 
+-- Create Development Database
 DROP DATABASE IF EXISTS eventspotter_dev;
 CREATE DATABASE eventspotter_dev;
 \connect eventspotter_dev;
 
+-- Apply Schema and Seed Data to Development Database
 \i eventspotter-schema.sql;
 \i eventspotter-seed.sql;
 
+-- Create Test Database
 DROP DATABASE IF EXISTS eventspotter_test;
 CREATE DATABASE eventspotter_test;
 \connect eventspotter_test;
 
+-- Apply Schema and Seed Data to Test Database
 \i eventspotter-schema.sql;
 \i eventspotter-seed.sql;
