d35b02881ae80900243507b92b02aa72afd7b8f5
ryzac <rzc044@gmail.com>
2024-06-07 03:00:18 -0500

    Updated password to be compliant with validation criteria from userModelValidation.

--- backend/db/eventspotter-seed.sql
+++ backend/db/eventspotter-seed.sql
@@ -1,9 +1,9 @@
--- both users have the password "password"
+-- both users have the password "Password!2"
 
 INSERT INTO users (username, password, email, is_admin, latitude, longitude, street, city, state, post_code, country)
 VALUES (
         'username1',
-        '$argon2id$v=19$m=65536,t=3,p=4$hhZXak0/rdnXJp74dqwRYQ$hbjKzPh6s5UIr4hXZuF/ZVLwACb9jNhMeTYH8cGgFeU',
+        '$argon2id$v=19$m=65536,t=3,p=4$hXbTt62l+IHN0r6D8Tf8Uw$+HvQWHCDI3YYT+MaON08vkOzMyyQwANeCfIzZrFPaMg',
         'username1@email.com',
         FALSE,
         30.351470,
@@ -15,7 +15,7 @@ VALUES (
         'USA'),
         (
         'username2',
-        '$argon2id$v=19$m=65536,t=3,p=4$hhZXak0/rdnXJp74dqwRYQ$hbjKzPh6s5UIr4hXZuF/ZVLwACb9jNhMeTYH8cGgFeU',
+        '$argon2id$v=19$m=65536,t=3,p=4$hXbTt62l+IHN0r6D8Tf8Uw$+HvQWHCDI3YYT+MaON08vkOzMyyQwANeCfIzZrFPaMg',
         'username2@email.com',
         TRUE,
         30.351470,
