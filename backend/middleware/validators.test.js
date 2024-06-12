9fb4c02dab6b045b8f90bf65193b4a0b4bd38b69
ryzac <rzc044@gmail.com>
2024-06-11 21:02:32 -0500

    Changed tests to reflect changes to validators.js file.

--- backend/middleware/validators.test.js
+++ backend/middleware/validators.test.js
@@ -1,132 +1,99 @@
 "use strict";
 
-const {
-    validateRegistrationFields,
-    validateLoginFields,
-    validateUsername,
-    validatePassword,
-    validateEmail,
-    validateIsAdmin
-} = require("./validators");
-
 const { BadRequestError } = require("../utils/expressError");
+const { validateUserRegistration } = require("./validators");
 
-describe("Validators", () => {
-    describe("validateRegistrationFields", () => {
-        test("should pass if all required fields are present", () => {
-            const req = { body: { username: "user", password: "Password!2", email: "user@example.com" } };
-            const next = jest.fn();
-            expect(() => validateRegistrationFields(req, {}, next)).not.toThrow();
-            expect(next).toHaveBeenCalled();
-        });
+describe("User Registration Validation Middleware", () => {
+    let req, res, next;
 
-        test("throws error if username is missing", () => {
-            const req = { body: { password: "Password!2", email: "user@example.com" } };
-            const next = jest.fn();
+    beforeEach(() => {
+        req = {
+            body: {}
+        };
+        res = {};
+        next = jest.fn();
+    });
+    
+    afterEach(() => {
+        req = {};
+        res = {};
+        next.mockReset();
+    });
+
+    test("Valid user registration passes validation", () => {
+        req.body = {
+            username: "user",
+            password: "Password!2",
+            email: "user@example.com"
+        }
+        expect(() => validateUserRegistration(req, res, next)).not.toThrow();
+        expect(next).toHaveBeenCalled();
+    });
+
+    describe("Validate that required fields are present for registration", () => {
+        test("throws BadRequestError if username missing", () => {
+            req.body = {
+                password: "Password!2",
+                email: "user@example.com"
+            };
             try {
-                validateRegistrationFields(req, {}, next);
+                validateUserRegistration(req, res, next);
             } catch (error) {
                 expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("User data missing for registration: username.")
+                expect(error.message).toBe("Username is required for registration.");
                 expect(next).not.toHaveBeenCalled();
             }
         });
 
-        test("throws error if password is missing", () => {
-            const req = { body: { username: "user", email: "user@example.com" } };
-            const next = jest.fn();
+        test("throws BadRequestError if password missing", () => {
+            req.body = {
+                username: "user",
+                email: "user@example.com"
+            };
             try {
-                validateRegistrationFields(req, {}, next);
+                validateUserRegistration(req, res, next);
             } catch (error) {
                 expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("User data missing for registration: password.")
+                expect(error.message).toBe("Password is required for registration.");
                 expect(next).not.toHaveBeenCalled();
             }
         });
 
-        test("throws error if email is missing", () => {
-            const req = { body: { username: "user", password: "Password!2" } };
-            const next = jest.fn();
+        test("throws BadRequestError if email missing", () => {
+            req.body = {
+                username: "user",
+                password: "Password!2"
+            };
             try {
-                validateRegistrationFields(req, {}, next);
+                validateUserRegistration(req, res, next);
             } catch (error) {
                 expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("User data missing for registration: email.")
+                expect(error.message).toBe("Email is required for registration.");
                 expect(next).not.toHaveBeenCalled();
             }
         });
 
-        test("throws error if all required fields are missing", () => {
-            const req = { body: { username: "", password: "", email: "" } };
-            const next = jest.fn();
+        test("throws BadRequestError if multiple fields missing", () => {
+            req.body = {};
             try {
-                validateRegistrationFields(req, {}, next);
+                validateUserRegistration(req, res, next);
             } catch (error) {
                 expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("User data missing for registration: username, password, email.")
+                expect(error.message).toBe("Username is required for registration. Password is required for registration. Email is required for registration.");
                 expect(next).not.toHaveBeenCalled();
             }
         });
     });
 
-    describe("validateLoginFields", () => {
-        test("should pass if all required fields are present", () => {
-            const req = { body: { username: "user", password: "Password!2" } };
-            const next = jest.fn();
-            expect(() => validateLoginFields(req, {}, next)).not.toThrow();
-            expect(next).toHaveBeenCalled();
-        });
-
-        test("throws error if username is missing", () => {
-            const req = { body: { password: "Password!2" } };
-            const next = jest.fn();
-            try {
-                validateLoginFields(req, {}, next);
-            } catch (error) {
-                expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("User data missing for login: username.")
-                expect(next).not.toHaveBeenCalled();
+    describe("Validate that username field has proper format", () => {
+        test("throws BadRequestError if username is too short", () => {
+            req.body = {
+                username: "us",
+                password: "Password!2",
+                email: "user@example.com"
             }
-        });
-
-        test("throws error if password is missing", () => {
-            const req = { body: { username: "user" } };
-            const next = jest.fn();
             try {
-                validateLoginFields(req, {}, next);
-            } catch (error) {
-                expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("User data missing for login: password.")
-                expect(next).not.toHaveBeenCalled();
-            }
-        });
-
-        test("throws error if both required fields are missing", () => {
-            const req = { body: { username: "", password: "" } };
-            const next = jest.fn();
-            try {
-                validateLoginFields(req, {}, next);
-            } catch (error) {
-                expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("User data missing for login: username, password.")
-                expect(next).not.toHaveBeenCalled();
-            }
-        });
-    });
-
-    describe("validateUsername", () => {
-        test("should pass if username is valid", () => {
-            const req = { body: { username: "valid_username" } };
-            const next = jest.fn();
-            expect(() => validateUsername(req, {}, next)).not.toThrow();
-            expect(next).toHaveBeenCalled();
-        });
-
-        test("throws error if username is too short", () => {
-            const req = { body: { username: "a" } };
-            const next = jest.fn();
-            try {
-                validateUsername(req, {}, next);
+                validateUserRegistration(req, res, next);
             } catch (error) {
                 expect(error).toBeInstanceOf(BadRequestError);
                 expect(error.message).toBe("Username must be at least 3 characters long.");
@@ -134,32 +101,46 @@ describe("Validators", () => {
             }
         });
 
-        test("throws error if username contains invalid characters", () => {
-            const req = { body: { username: "invalid&!usern@me" } };
-            const next = jest.fn();
+        test("throws BadRequestError if username is too long", () => {
+            req.body = {
+                username: "u".repeat(51),
+                password: "Password!2",
+                email: "user@example.com"
+            }
             try {
-                validateUsername(req, {}, next);
+                validateUserRegistration(req, res, next);
             } catch (error) {
                 expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("Username can only contain alphanumeric characters and underscores. Invalid characters found: &, !, @");
+                expect(error.message).toBe("Username cannot exceed 50 characters.");
+                expect(next).not.toHaveBeenCalled();
+            }
+        });
+        
+        test("throws BadRequestError if username contains invalid characters", () => {
+            req.body = {
+                username: "user@name",
+                password: "Password!2",
+                email: "user@example.com"
+            };
+            try {
+                validateUserRegistration(req, res, next);
+            } catch (error) {
+                expect(error).toBeInstanceOf(BadRequestError);
+                expect(error.message).toBe("Username can only contain alphanumeric characters and underscores.");
                 expect(next).not.toHaveBeenCalled();
             }
         });
     });
 
-    describe("validatePassword", () => {
-        test("does not throw error if password is valid", () => {
-            const req = { body: { password: "Password!2" } };
-            const next = jest.fn();
-            expect(() => validatePassword(req, {}, next)).not.toThrow();
-            expect(next).toHaveBeenCalled();
-        });
-
-        test("throws error if password is too short", () => {
-            const req = { body: { password: "Pas!2" } };
-            const next = jest.fn();
+    describe("Validate that password field has proper format", () => {
+        test("throws BadRequestError if password is too short", () => {
+            req.body = {
+                username: "user",
+                password: "Pwd!2",
+                email: "user@example.com"
+            }
             try {
-                validatePassword(req, {}, next);
+                validateUserRegistration(req, res, next);
             } catch (error) {
                 expect(error).toBeInstanceOf(BadRequestError);
                 expect(error.message).toBe("Password must be at least 6 characters long.");
@@ -167,92 +148,64 @@ describe("Validators", () => {
             }
         });
 
-        test("throws error if password does not contain following: uppercase, digit, special character", () => {
-            const req = { body: { password: "password" } };
-            const next = jest.fn();
+        test("throws BadRequestError if password is too long", () => {
+            req.body = {
+                username: "user",
+                password: "Password!2".repeat(6),
+                email: "user@example.com"
+            }
             try {
-                validatePassword(req, {}, next);
+                validateUserRegistration(req, res, next);
             } catch (error) {
                 expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("Password must include at least one uppercase letter, digit, special character.");
+                expect(error.message).toBe("Password cannot exceed 50 characters.");
                 expect(next).not.toHaveBeenCalled();
             }
         });
-
-        test("throws error if password does not contain following: lowercase, digit, special character", () => {
-            const req = { body: { password: "PASSWORD" } };
-            const next = jest.fn();
+        
+        test("throws BadRequestError if password does not contains required special characters", () => {
+            req.body = {
+                username: "user",
+                password: "Password2",
+                email: "user@example.com"
+            };
             try {
-                validatePassword(req, {}, next);
+                validateUserRegistration(req, res, next);
             } catch (error) {
                 expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("Password must include at least one lowercase letter, digit, special character.");
-                expect(next).not.toHaveBeenCalled();
-            }
-        });
-
-        test("throws error if password does not contain following: digit, special character", () => {
-            const req = { body: { password: "Password" } };
-            const next = jest.fn();
-            try {
-                validatePassword(req, {}, next);
-            } catch (error) {
-                expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("Password must include at least one digit, special character.");
-                expect(next).not.toHaveBeenCalled();
-            }
-        });
-
-        test("throws error if password does not contain following: special character", () => {
-            const req = { body: { password: "Password1" } };
-            const next = jest.fn();
-            try {
-                validatePassword(req, {}, next);
-            } catch (error) {
-                expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("Password must include at least one special character.");
+                expect(error.message).toBe("Password must include at least one uppercase letter, one lowercase letter, one digit, and one special character.");
                 expect(next).not.toHaveBeenCalled();
             }
         });
     });
 
-    describe("validateEmail", () => {
-        test("should pass if email format is valid", () => {
-            const req = { body: { email: "test@example.com" } };
-            const next = jest.fn();
-            expect(() => validateEmail(req, {}, next)).not.toThrow();
-            expect(next).toHaveBeenCalled();
+    describe("Validate that email field has proper format", () => {
+        test("throws BadRequestError if email is too long", () => {
+            req.body = {
+                username: "user",
+                password: "Password!2",
+                email: "u".repeat(100) + "@example.com"
+            }
+            try {
+                validateUserRegistration(req, res, next);
+            } catch (error) {
+                expect(error).toBeInstanceOf(BadRequestError);
+                expect(error.message).toBe("Email cannot exceed 100 characters.");
+                expect(next).not.toHaveBeenCalled();
+            }
         });
 
-        test("throws error if email format is not valid", () => {
-            const req = { body: { email: "test!example.com" } };
-            const next = jest.fn();
+        test("throws BadRequestError if email format is invalid", () => {
+            req.body = {
+                username: "user",
+                password: "Password!2",
+                email: "user!example.com"
+            }
             try {
-                validateEmail(req, {}, next);
+                validateUserRegistration(req, res, next);
             } catch (error) {
                 expect(error).toBeInstanceOf(BadRequestError);
                 expect(error.message).toBe("Invalid email format.");
-                expect(next).not.toHaveBeenCalled();
-            }
-        });
-    });
-
-    describe("validateIsAdmin", () => {
-        test("should pass if isAdmin is a boolean", () => {
-            const req = { body: { isAdmin: true } };
-            const next = jest.fn();
-            expect(() => validateIsAdmin(req, {}, next)).not.toThrow();
-            expect(next).toHaveBeenCalled();
-        })
-
-        test("throws error if isAdmin is not a boolean", () => {
-            const req = { body: { isAdmin: "not_a_boolean" } };
-            const next = jest.fn();
-            try {
-                validateIsAdmin(req, {}, next);
-            } catch (error) {
-                expect(error).toBeInstanceOf(BadRequestError);
-                expect(error.message).toBe("Invalid value for isAdmin. Must be boolean true or false.");
                 expect(next).not.toHaveBeenCalled();
             }
         });
