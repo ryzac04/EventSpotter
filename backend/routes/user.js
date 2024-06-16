d32f796f408aaff86f2b61e60335cdd10a374cc8
ryzac <rzc044@gmail.com>
2024-06-16 02:44:08 -0500

    Added ensureAuthenticated middleware to user routes.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,16 +3,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { validateUserRegistration } = require("../middleware/validators");
+const { validateUserRegistration, validateUserUpdate } = require("../middleware/validators");
 const { ensureAdmin, ensureAuthenticated, ensureCorrectUserOrAdmin } = require("../middleware/user");
 const { registerUser } = require("../controllers/auth");
 const { findAllUsers, findUser, updateUser, deleteUser } = require("../controllers/user");
 
 // User Routes 
-router.post("/", ensureAdmin, validateUserRegistration, registerUser);
-router.get("/", ensureAdmin, findAllUsers);
+router.post("/", ensureAuthenticated, ensureAdmin, validateUserRegistration, registerUser);
+router.get("/", ensureAuthenticated, ensureAdmin, findAllUsers);
 router.get("/:username", ensureAuthenticated, ensureCorrectUserOrAdmin, findUser);
-router.patch("/:username", ensureAuthenticated, ensureCorrectUserOrAdmin, updateUser);
+router.patch("/:username", ensureAuthenticated, ensureCorrectUserOrAdmin, validateUserUpdate, updateUser);
 router.delete("/:username", ensureAuthenticated, ensureCorrectUserOrAdmin, deleteUser);
 
 module.exports = router;
