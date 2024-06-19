e2061c836fbc1e234c6b16f72307341f0063eebd
ryzac <rzc044@gmail.com>
2024-06-18 22:39:42 -0500

    LoginForm component initial commit.

---
+++ frontend/src/components/auth/LoginForm.js
@@ -0,0 +1,79 @@
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Alert from "../common/Alert";
+
+const LoginForm = ({ login }) => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        username: "",
+        password: ""
+    });
+    const [formErrors, setFormErrors] = useState([]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const result = await login(formData);
+            if (result.success) {
+                navigate("/");
+            } else {
+                setFormErrors(result.error);
+            }
+        } catch (error) {
+            console.error("Error during signup:", error);
+        }
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value })
+    }
+
+    return (
+        <div className="SignupForm">
+            <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+                <h2 className="mb-3">Sign Up</h2>
+                <div className="card">
+                    <div className="card-body">
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <label>Username</label>
+                                <input
+                                    name="username"
+                                    className="form-control"
+                                    value={formData.username}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className="form-control"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            {formErrors.length
+                                ? <Alert type="danger" messages={formErrors} />
+                                : null
+                            }
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary float-right"
+                            >
+                                Submit
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginForm;
