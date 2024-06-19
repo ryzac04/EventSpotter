84df9206c6679223460676a7cb1e17179a6edcdd
ryzac <rzc044@gmail.com>
2024-06-19 04:53:01 -0500

    added storeRefreshToken method to registerUser and authenticateUser. Added logoutUser function.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -11,20 +11,24 @@ const {createAccessToken, createRefreshToken, verifyToken} = require("../utils/j
 
 async function registerUser(req, res, next) {
     try {
-        const { username, password, email, isAdmin } = req.body;
+        const { username, password, email, isAdmin = false } = req.body;
         const newUser = await User.register({ username, password, email, isAdmin });
 
         // Set newUser to res.locals.user for authorization later
         res.locals.user = newUser;
 
+        // Create tokens
         const accessToken = createAccessToken(newUser);
         const refreshToken = createRefreshToken(newUser);
+
+        // Store refresh token in database
+        await User.storeRefreshToken(newUser.id, refreshToken);
 
         // Set tokens in the response header
         res.header("Authorization", accessToken).header("x-refresh-token", refreshToken);
         
         // Return registered user in the response body
-        return res.status(201).json(newUser);
+        return res.status(201).json({ newUser, accessToken, refreshToken });
     } catch (error) {
         next(error); 
     };
@@ -38,14 +42,18 @@ async function authenticateUser(req, res, next) {
         // Set authUser to res.locals.user for authorization later
         res.locals.user = authUser;
 
+        // Create tokens 
         const accessToken = createAccessToken(authUser);
         const refreshToken = createRefreshToken(authUser);
+        
+        // Store refresh token in database
+        await User.storeRefreshToken(authUser.id, refreshToken);
 
         // Set tokens in the response header
         res.header("Authorization", accessToken).header("x-refresh-token", refreshToken);
 
         // Return logged in user in the response body
-        return res.status(200).json(authUser);
+        return res.status(200).json({ authUser, accessToken, refreshToken });
     } catch (error) {
         next(error);
     };
@@ -66,8 +74,22 @@ async function refreshToken(req, res, next) {
     };
 };
 
+async function logoutUser(req, res, next) {
+    try {
+        const refreshToken = req.headers["x-refresh-token"];
+        console.log("REFRESH TOKEN:", refreshToken);
+
+        await User.deleteRefreshToken(refreshToken);
+
+        return res.status(200).json({ success: true, message: "User logged out successfully" });
+    } catch (error) {
+        next(error);
+    };
+};
+
 module.exports = {
     registerUser,
     authenticateUser,
-    refreshToken
+    refreshToken,
+    logoutUser
 };
