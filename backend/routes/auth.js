b3cd8033c0588227fbf5d121b6979b405db24f9b
ryzac <rzc044@gmail.com>
2024-06-19 04:54:09 -0500

    added /logout route.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,11 +5,12 @@ const router = express.Router();
 
 const {validateUserRegistration, validateUserAuth} = require("../middleware/validators");
 const { validateRefreshToken } = require("../middleware/auth");
-const { registerUser, authenticateUser, refreshToken } = require("../controllers/auth");
+const { registerUser, authenticateUser, refreshToken, logoutUser } = require("../controllers/auth");
 
 // Auth Routes
 router.post("/register", validateUserRegistration, registerUser);
 router.post("/login", validateUserAuth, authenticateUser);
 router.post("/refresh", validateRefreshToken, refreshToken);
+router.post("/logout", logoutUser);
 
 module.exports = router;
