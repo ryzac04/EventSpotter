c5e72a67891ed39d19219cacc35b43139a2a24f4
ryzac <rzc044@gmail.com>
2024-06-22 03:56:25 -0500

    Unfinished block comment, updated entire component to correctly display content upon login or registration.

--- frontend/src/components/homepage/Homepage.js
+++ frontend/src/components/homepage/Homepage.js
@@ -1,10 +1,43 @@
 
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../../contexts/authContext";
+
+/**
+ * Homepage Component
+ * 
+ * 
+ * route: / 
+ */
+
 const Homepage = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
-};
+  const { currentUser } = useAuthContext();
+  console.debug("Homepage", "currentUser=", currentUser);
+
+  return (
+    <div className="Homepage">
+      <div className="container text-center">
+        <h1 className="mb-4 font-weight-bold">EventSpotter</h1>
+        <p className="lead">Fun at your fingertips.</p>
+        {currentUser
+          ? <h2>
+            Welcome Back, {currentUser.username}!
+          </h2>
+          : (
+            <p>
+              <Link className="btn btn-primary font-weight-bold mr-3"
+                to="/login">
+                Log in
+              </Link>
+              <Link className="btn btn-primary font-weight-bold"
+                to="/signup">
+                Sign up
+              </Link>
+            </p>
+          )
+        }
+      </div>
+    </div>
+  );
+}
 
 export default Homepage;
