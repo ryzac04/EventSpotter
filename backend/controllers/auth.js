59bcc49a1d5fbbeb3eb8e7be352ffb27fff5dcc0
ryzac <rzc044@gmail.com>
2024-06-22 03:37:46 -0500

    Removed user from returned response objects - only responding with JWTs now.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,6 @@
 "use strict";
 
 require("dotenv").config();
-
 const { REFRESH_JWT_SECRET } = process.env;
 
 const User = require("../models/user");
@@ -12,23 +11,23 @@ const {createAccessToken, createRefreshToken, verifyToken} = require("../utils/j
 async function registerUser(req, res, next) {
     try {
         const { username, password, email, isAdmin = false } = req.body;
-        const newUser = await User.register({ username, password, email, isAdmin });
+        const user = await User.register({ username, password, email, isAdmin });
 
-        // Set newUser to res.locals.user for authorization later
-        res.locals.user = newUser;
+        // Set user to res.locals.user for authorization later
+        res.locals.user = user;
 
         // Create tokens
-        const accessToken = createAccessToken(newUser);
-        const refreshToken = createRefreshToken(newUser);
+        const accessToken = createAccessToken(user);
+        const refreshToken = createRefreshToken(user);
 
         // Store refresh token in database
-        await User.storeRefreshToken(newUser.id, refreshToken);
+        await User.storeRefreshToken(user.id, refreshToken);
 
         // Set tokens in the response header
         res.header("Authorization", accessToken).header("x-refresh-token", refreshToken);
         
-        // Return registered user in the response body
-        return res.status(201).json({ newUser, accessToken, refreshToken });
+        // Return tokens in the response body
+        return res.status(201).json({ accessToken, refreshToken });
     } catch (error) {
         next(error); 
     };
@@ -37,23 +36,23 @@ async function registerUser(req, res, next) {
 async function authenticateUser(req, res, next) {
     try {
         const { username, password } = req.body;
-        const authUser = await User.authenticate(username, password);
+        const user = await User.authenticate(username, password);
 
-        // Set authUser to res.locals.user for authorization later
-        res.locals.user = authUser;
+        // Set user to res.locals.user for authorization later
+        res.locals.user = user;
 
         // Create tokens 
-        const accessToken = createAccessToken(authUser);
-        const refreshToken = createRefreshToken(authUser);
+        const accessToken = createAccessToken(user);
+        const refreshToken = createRefreshToken(user);
         
         // Store refresh token in database
-        await User.storeRefreshToken(authUser.id, refreshToken);
+        await User.storeRefreshToken(user.id, refreshToken);
 
         // Set tokens in the response header
         res.header("Authorization", accessToken).header("x-refresh-token", refreshToken);
 
-        // Return logged in user in the response body
-        return res.status(200).json({ authUser, accessToken, refreshToken });
+        // Return tokens in the response body
+        return res.status(200).json({ accessToken, refreshToken });
     } catch (error) {
         next(error);
     };
@@ -77,8 +76,7 @@ async function refreshToken(req, res, next) {
 async function logoutUser(req, res, next) {
     try {
         const refreshToken = req.headers["x-refresh-token"];
-        console.log("REFRESH TOKEN:", refreshToken);
-
+        
         await User.deleteRefreshToken(refreshToken);
 
         return res.status(200).json({ success: true, message: "User logged out successfully" });
