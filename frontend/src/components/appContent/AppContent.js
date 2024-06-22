bfc140208749c446cfbe3e759da4dc1a1b953fe3
ryzac <rzc044@gmail.com>
2024-06-22 03:54:18 -0500

    Initial commit.

---
+++ frontend/src/components/appContent/AppContent.js
@@ -0,0 +1,35 @@
+
+import AppRoutes from "../routes-nav/AppRoutes";
+import NavBar from "../routes-nav/NavBar";
+import LoadingSpinner from "../common/LoadingSpinner";
+import { useAuthContext } from "../../contexts/authContext";
+
+/**
+ * AppContent Component
+ * 
+ * Responsible for rendering the main content of the application after authentication information (`infoLoaded`) has been loaded.
+ * 
+ * Integrates with user authentication context (`useAuthContext`) to manage
+ * authentication-related actions and render navigation (`NavBar`) and
+ * application routes (`AppRoutes`).
+ * 
+ * Uses `LoadingSpinner` to indicate loading state until authentication
+ * information is ready.
+ */
+
+const AppContent = () => {
+  const { infoLoaded, logout, signup, login } = useAuthContext();
+
+  if (!infoLoaded) return <LoadingSpinner />;
+
+  return (
+    <>
+      <NavBar logout={logout} />
+      <main className="Container pt-5">
+        <AppRoutes signup={signup} login={login} />
+      </main>
+    </>
+  );
+}
+
+export default AppContent;
