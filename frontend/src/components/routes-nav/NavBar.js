96f2499bb63dadbb925c3beb4e0b51f4c9c7a5a9
ryzac <rzc044@gmail.com>
2024-06-22 03:53:53 -0500

    Initial commit.

---
+++ frontend/src/components/routes-nav/NavBar.js
@@ -0,0 +1,65 @@
+
+import { NavLink, Link } from "react-router-dom";
+import { useAuthContext } from "../../contexts/authContext";
+
+import "./NavBar.css";
+
+/**
+ * NavBar Component
+ * 
+ * Navigation bar for site - shows up on every page. 
+ * 
+ * Logged out user shows links to login and signup forms. 
+ * Logged in user shows username as link to profile page and logout button.
+ */
+
+const NavBar = ({logout}) => {
+    const { currentUser } = useAuthContext();
+    console.debug("Navigation", "currentUser=", currentUser);
+    
+    const loggedInNav = () => {
+        return (
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item mr-4">
+                    <NavLink className="nav-link" to="/profile">
+                        Profile
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/" onClick={logout}>
+                        Log out {currentUser.username}
+                    </Link>
+                </li>
+            </ul>
+        );
+    }
+
+    const loggedOutNav = () => {
+        return (
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item me-4">
+                    <NavLink className="nav-link" to="/login">
+                        Login
+                    </NavLink>
+                </li>
+                <li className="nav-item me-4">
+                    <NavLink className="nav-link" to="/signup">
+                        Signup
+                    </NavLink>
+                </li>
+            </ul>
+        );
+    }
+
+    return (
+        <nav className="Navigation navbar navbar-expand-md">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">EventSpotter</Link>
+                {currentUser ? loggedInNav() : loggedOutNav()}
+            </div>
+        </nav>
+    );
+};
+
+
+export default NavBar;
