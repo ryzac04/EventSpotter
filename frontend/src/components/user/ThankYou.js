9097ea34497cbc54a192d09d24ca1b583f9954e8
ryzac <rzc044@gmail.com>
2024-06-26 08:48:38 -0500

    Initial Commit.

---
+++ frontend/src/components/user/ThankYou.js
@@ -0,0 +1,15 @@
+
+const ThankYou = () => {
+    return (
+        <div className="ThankYou col-md-6 col-lg-4 offset-md-3 offset-lg-4">
+            <h3>Account Deleted</h3>
+            <div className="card">
+                <div className="card-body">
+                    <p>Your account has been successfully deleted. Thank you for using our services! Please consider us in the future if you ever need our services again!</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ThankYou;
