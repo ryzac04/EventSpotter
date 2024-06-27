b8d94aa979c5afd7b383eaa9c03dd5ee4aeb4617
ryzac <rzc044@gmail.com>
2024-06-27 02:16:05 -0500

    Modified this component to correctly utilize private routes.

--- frontend/src/components/routes-nav/PrivateRoute.js
+++ frontend/src/components/routes-nav/PrivateRoute.js
@@ -1,15 +1,12 @@
 
 import React from "react";
-import { Route, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../../contexts/authContext";
 
-const PrivateRoute = ({ path, ...props }) => {
-    const { isAuthenticated } = useAuthContext();
-
-    return isAuthenticated ? (
-        <Route {...props} path={path} />
-    ) : (
-        <Navigate to="/login" replace state={{ from: path }} />
+const PrivateRoute = ({ children }) => {
+    const { currentUser } = useAuthContext();
+    return (
+        currentUser ? <>{children} </> : <Navigate to="/login" />
     );
 };
 
