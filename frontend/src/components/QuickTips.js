b3b77009abe460a9defbadcf75f32287a6df1036
ryzac <rzc044@gmail.com>
2024-07-11 17:09:03 -0500

    Adjusted formatting and added block comment.

--- frontend/src/components/QuickTips.js
+++ frontend/src/components/QuickTips.js
@@ -1,16 +1,41 @@
 
+/** Disclaimer, quick tips, and information that may make the user's life a little easier. */
+
 const QuickTips = () => {
     return (
-        <div>
-            <p>The event are all there, it's just that some venues may have multiple events at it.</p>
-            <p>Hey there! Just a quick heads up that some dates, times, and/or prices may not be available, especially for events farther out. We do our best to keep things current and accurate but if you notice any missing details, please double-check via the Purchase Tickets link provided. Stay safe and have fun!</p>
-            <ul>
-                <li>We'll ask for your current location to help you find fun events near you.</li>
-                <li>If you don't want to give permission to use your current location, no problem! You can still use our map to find fun things to do. Just find a place you'd like to search for fun things to do, fill out as much information as you can in our search form, and search away! There's a little map scale at the bottom - click on it to toggle between miles and kilometers. Zoom in a bit on the place you'd like to search and try to place it as close the center of the map as you can - doesn't have to be exact. For a little extra help, place a value in the Distance input bar. It goes by miles (sorry Europe) and will search in a circle around the center of the map. Have fun!</li>
-                <li>Dropped pin - try to drop pins where there are valid addresses. If you drop it somewhere and get an address that looks something like this: ABC123+AB12 (or something of that nature), that's called a Plus Code. These are part of a digital address system developed by Google to represent locations that do not have a specific street address - fun fact maybe? In any case, if you're trying to use a location like this as the origin of a direction route, it may not work - it should! But it might not...  </li>
-            </ul>
+        <div className="QuickTips">
+            <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+                <h2 className="heading-text">A Few Things Worth Mentioning...</h2>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-subtitle mb-2 text-muted">Disclaimer:</h5>
+                        <p>Some dates, times, locations and/or prices for events may not be available - especially if an event date is farther out. We strive to keep information accurate, but please double-check everything and take a look at the Purchase Tickets link on the map page. Stay safe and have fun!</p>
+                        <hr/>
+                        <h5 className="card-subtitle mb-2 text-muted">Quick Tips:</h5>
+                        <ul>
+                            <li>We'll ask for your location to help find nearby events. If you prefer not to share, you can still use the map to search for fun activities.</li>
+                            <li>To search without using your location:
+                                <ul>
+                                    <li>Use the map to find an area you want to explore.</li>
+                                    <li>Drop a pin on the map - this is like a custom origin pin instead of your location.</li>
+                                    <li>Fill out the form and search away!</li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <hr />
+                        <h5 className="card-subtitle mb-2 text-muted">Other Pro Tips:</h5>
+                        <ul className="mb-0">
+                            <li>The Distance searched is in a radius around either your location, a dropped pin, or around the center point of wherever the map is (no pin required).</li>
+                            <li>The Distance search is measured in miles - sorry Europe.</li>
+                            <li>We included a little scale at the bottom to help with distances - click on it to switch between miles and kilometers.</li>
+                            <li>If you drop a pin, try to do so where there are valid addresses. Pins may show a Plus Code (e.g., ABC123+AB12) which is a digital address by Google for places without street addresses. These may not always work for directions, but they usually do.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 };
-            
+
 export default QuickTips;
