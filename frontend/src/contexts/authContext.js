ae9b0229f4cfd1caa54f3db28e20cee59417711a
ryzac <rzc044@gmail.com>
2024-07-11 17:55:40 -0500

    Adjusted documentation.

--- frontend/src/contexts/authContext.js
+++ frontend/src/contexts/authContext.js
@@ -4,19 +4,16 @@ import useAuth from "../hooks/useAuth";
 
 /**
  * AuthContext
- * 
  * Provides authentication-related data and methods to components throughout the application.
  * It uses the useAuth hook to manage authentication state and actions.
  * 
  * AuthProvider Component
- * 
  * Wraps its children with AuthContext.Provider and supplies the authentication state and methods.
  * 
  * Props:
  * - children: the components that will be wrapped by the AuthProvider.
  * 
  * useAuthContext Hook
- * 
  * A custom hook that allows components to easily consume the authentication context.
  */
 
