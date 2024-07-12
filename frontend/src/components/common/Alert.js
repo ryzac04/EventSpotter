7e56a01ca45642c2f35c5ac9fc2165416a5cc574
ryzac <rzc044@gmail.com>
2024-07-12 17:41:22 -0500

    Added functionality to turn alert message into array that can be turned into a list.

--- frontend/src/components/common/Alert.js
+++ frontend/src/components/common/Alert.js
@@ -24,13 +24,15 @@ const Alert = ({ type = "danger", messages = [] }) => {
     if (!error && messages.length === 0) return null;
 
     const errorMessages = error ? [error] : messages;
-
+    const errorsList = errorMessages[0].split(". ");
     return (
         <div className="alert-container mt-4">
             <div className={`alert alert-${type}`} role="alert">
-                {errorMessages.map((errorMessage, index) => (
-                    <p className="mb-0 small" key={index}>{errorMessage}</p>
-                ))}
+                <ul className="list-unstyled mb-0">
+                    {errorsList.map((error, index) => (
+                        <li className="mb-0 small" key={index}>{error}</li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
