235c48c1b50440d3994485fd35adbd593dda2cb0
ryzac <rzc044@gmail.com>
2024-07-12 15:52:10 -0500

    Added EventProvider and documentation.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
 
 import { BrowserRouter } from "react-router-dom";
 
-import { AuthProvider } from "./contexts/authContext";
+import { ErrorProvider } from "./contexts/ErrorContext";
+import { AuthProvider } from "./contexts/AuthContext";
 import AppContent from "./components/appContent/AppContent";
 
 import './App.css';
 
-/** EventSpotter Application */
+/** 
+ * EventSpotter Application 
+ * 
+ * Main entry point of the EventSpotter application.
+ * Provides routing using BrowserRouter and wraps the entire application with ErrorProvider and AuthProvider to manage error states and authentication context throughout the app. 
+*/
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <BrowserRouter>
-                    <AuthProvider>
-                        <AppContent /> 
-                    </AuthProvider>
+                    <ErrorProvider>
+                        <AuthProvider>
+                            <AppContent />
+                        </AuthProvider>
+                    </ErrorProvider>
                 </BrowserRouter>
             </header>
         </div>
     );
-}
+};
 
 export default App;
