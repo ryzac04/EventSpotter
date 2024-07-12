0c1eba52bdc821473abeed76c8a78980eed78ec5
ryzac <rzc044@gmail.com>
2024-07-12 15:53:15 -0500

    Modified code for smoother use with ErrorProvider and added documentation.

--- frontend/src/components/common/Alert.js
+++ frontend/src/components/common/Alert.js
@@ -1,17 +1,37 @@
 
+import React, { useContext } from "react";
+import { ErrorContext } from "../../contexts/ErrorContext";
+
+import "./Alert.css";
+
 /** Alert Component
  * 
  * Presentational component for bootstrap-style alerts. 
  * 
- * Found in: SignupForm.js, LoginForm.js, Profile.js, EventFilterForm.js
+ * Found in: 
+ * - SignupForm.js
+ * - LoginForm.js
+ * - Profile.js
+ * - PermissionModal.js
+ * - DroppedPin.js
+ * - AutocompleteSearch.js
+ * - Directions.js
+ * - EventFilterForm.js
  */ 
 
 const Alert = ({ type = "danger", messages = [] }) => {
+    const { error } = useContext(ErrorContext);
+    if (!error && messages.length === 0) return null;
+
+    const errorMessages = error ? [error] : messages;
+
     return (
-        <div className={`alert alert-${type}`} role="alert">
-            {messages.map(error => (
-                <p className="mb-0 small" key={error}>{error}</p>
-            ))}
+        <div className="alert-container mt-4">
+            <div className={`alert alert-${type}`} role="alert">
+                {errorMessages.map((errorMessage, index) => (
+                    <p className="mb-0 small" key={index}>{errorMessage}</p>
+                ))}
+            </div>
         </div>
     );
 };
