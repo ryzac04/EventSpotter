3ce429d06ff6aa93a8fe568a5e56448336be719c
ryzac <rzc044@gmail.com>
2024-07-12 23:03:15 -0500

    Included formatting utility function and modified documentation.

--- frontend/src/components/common/Alert.js
+++ frontend/src/components/common/Alert.js
@@ -1,35 +1,29 @@
 
 import React, { useContext } from "react";
 import { ErrorContext } from "../../contexts/ErrorContext";
-
-import "./Alert.css";
+import { formatErrorMessages } from "../../utils/errorUtils";
 
 /** Alert Component
  * 
  * Presentational component for bootstrap-style alerts. 
  * 
  * Found in: 
+ * - EventMap.js 
  * - SignupForm.js
  * - LoginForm.js
  * - Profile.js
- * - PermissionModal.js
- * - DroppedPin.js
- * - AutocompleteSearch.js
- * - Directions.js
- * - EventFilterForm.js
  */ 
 
 const Alert = ({ type = "danger", messages = [] }) => {
     const { error } = useContext(ErrorContext);
     if (!error && messages.length === 0) return null;
 
-    const errorMessages = error ? [error] : messages;
-    const errorsList = errorMessages[0].split(". ");
+    const errorMessages = formatErrorMessages(error);
     return (
         <div className="alert-container mt-4">
             <div className={`alert alert-${type}`} role="alert">
                 <ul className="list-unstyled mb-0">
-                    {errorsList.map((error, index) => (
+                    {errorMessages.map((error, index) => (
                         <li className="mb-0 small" key={index}>{error}</li>
                     ))}
                 </ul>
